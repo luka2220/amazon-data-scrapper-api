3d4108d42c3489ed9e2916aed1474e849ceca261
luka2220 <piplicaluka64@gmail.com>
2022-01-03 20:51:05 -0500

    product details and review completed, working on product offers

--- index.js
+++ index.js
@@ -5,12 +5,44 @@ const request = require('request-promise');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// scraperApi key
+const apiKey = '0eb542be9615de056212fe4ec0f66896';
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
 // allows the application to parse JSON input
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Welcome to Amazon Product Data Scraper');
+    res.send('Welcome to Amazon Product Data Scraper API');
 });
+
+// fetching product details
+app.get('/products/:productId', async (req, res) => {
+    const { productId } = req.params;
+
+    try {
+        // gets the data from the productId arg passed
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/dp/${productId}`);
+
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+})
+
+// fetching product reviews
+app.get('/products/:productId/reviews', async (req, res) => {
+    const { productId } = req.params;
+
+    try {
+        // gets the data from the productId arg passed
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`);
+
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+})
 
 // server will listen on port 5000
 app.listen(PORT, () => {
